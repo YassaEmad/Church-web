e76a064df7bb324101791ab67933945575e7cb3a
YassaEmad <yassaemad86@gmail.com>
2025-11-19 21:46:48 +0200

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import React, { Suspense } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+
 import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import { DataProvider } from "./contexts/DataContext";
 import Loading from "./components/Loading";
-import Footer from "./components/Footer";
+
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Footer = React.lazy(() => import("./components/Footer"));
 
 const Home = React.lazy(() => import("./pages/Home"));
 const News = React.lazy(() => import("./pages/News"));
